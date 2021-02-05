d9dd82471846dd6f822e28d4cc26dc4e4d529201
Ricardo <reconghostt@gmail.com>
2021-02-04 23:22:20 -0600

    Test App

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php
+$addressee = 'reonghostt@gmail.com';
+if (isset($_POST['send'])){
+    if(!empty($_POST['Nombre'] && !empty($_POST['Telefono']))){
+        $name = $_POST['Nombre'];
+        $phone = $_POST['Telefono'];
+
+
+        $mensaje = '
+<html>
+<head>
+  <title>Informacion</title>
+</head>
+<body>
+  <table>
+    <tr>
+      <th>Nombre</th><th>Telefono</th>
+    </tr>
+    <tr>
+      <td>$name</td><td>$phone</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+$header = 'Informacion';
+$header = 'From: Your name <info@address.com>';
+$header.= 'Reply-To: info@address.com'; 
+$header.= 'X-Mailer: PHP/'. phpversion();
+
+        mail($addressee, $name ,$phone , $header , $mensaje);
+    }
+}
+
+?>
