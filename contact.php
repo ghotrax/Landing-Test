bac58ada5e870fcc5c80280036c775c4ee18994d
Ricardo <reconghostt@gmail.com>
2021-02-05 00:17:30 -0600

    added contact.php file

--- contact.php
+++ contact.php
@@ -1,14 +1,12 @@
 <?php
 
-if (isset($_POST['send'])){
-    if(!empty($_POST['Nombre'] && !empty($_POST['Telefono']))){
         $addressee = 'reconghostt@gmail.com';
         $name = $_POST['Nombre'];
         $phone = $_POST['Telefono'];
         $mensaje = "DE: $name \n";
-        $header .= "From: Your name <info@address.com>";
+        $header.= "From: Your name <info@address.com>";
         $header.= "Reply-To: info@address.com"; 
-        $header.= "X-Mailer: PHP/". phpversion();
+      
 
       
 
@@ -16,7 +14,7 @@ if (isset($_POST['send'])){
 
         mail($addressee, $mensaje ,$header);
 
-    }
-}
+    
+
 
 ?>
